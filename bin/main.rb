4688c5ee7f719779d83c4a68d97e43812d27354f
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-07-28 22:50:12 +0300

    add user interface

--- bin/main.rb
+++ bin/main.rb
@@ -12,14 +12,21 @@ end
 puts 'Welcome to the Tic tac Toe field'
 
 puts 'Player 1: Enter Your Name'
-opponent_one = _input
+opponent_one = _input.capitalize
 puts 'Player 2: Enter Your Name'
-opponent_two = _input
+opponent_two = _input.capitalize
 
 puts '*********************'
-puts "#{opponent_one.capitalize} VS #{opponent_two.capitalize}"
+puts "#{opponent_one} VS #{opponent_two}"
 puts '*********************'
 
 puts '***** INSTRUCTION *****'
 puts "Enter the respective number to concure the space \n
-If one player write a straight line he/she/they would be a winner"
+If one player write a straight line he/she/they would be a winner \n"
+
+puts 'Here is the playing board select your move'
+puts print_board
+
+puts 'Enter Your Next Move'
+
+puts "Hooray! \n #{winner.name} win the game"
