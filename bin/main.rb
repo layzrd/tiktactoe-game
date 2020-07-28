9afbd946a8859c03b6035e0483af1914b5c58bc0
alexoid <alex_zam@hotmail.es>
2020-07-28 13:05:31 -0500

    add game interface

--- bin/main.rb
+++ bin/main.rb
@@ -8,8 +8,17 @@ def _input
   end
   inp
 end
-
+def playerMove (input)
+    if input.is_a?(Integer) &&  input > 0 && input < 10
+        puts 'ok'
+    else
+        puts 'invalid character, write a number between 1 to 9' 
+        move1=gets.chomp
+        playerMove(move1)
+    end        
+end    
 puts 'Welcome to the Tic tac Toe field'
+
 
 puts 'Player 1: Enter Your Name'
 opponent_one = _input
@@ -23,3 +32,16 @@ puts '*********************'
 puts '***** INSTRUCTION *****'
 puts "Enter the respective number to concure the space \n
 If one player write a straight line he/she/they would be a winner"
+puts 'Choose one of the spaces on the board'
+puts '1|2|3'
+puts '______'
+puts '4|5|6'
+puts '______'
+puts '7|8|9'
+puts "#{opponent_one} make your move"
+move1=gets.chomp.to_i
+
+playerMove(move1)
+
+
+
