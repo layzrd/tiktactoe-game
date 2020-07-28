f69684d09db1c47cc74f94e93505b0f8f4e3b338
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-07-28 20:37:03 +0300

    add user interface

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,23 @@
 #!/usr/bin/env ruby
 
-print 'Hello world'
+def _input
+  inp = gets.chomp
+  unless inp
+    puts 'Please enter a valid input'
+    getInput
+  end
+  inp
+end
+
+print 'Welcome to the Tic tac Toe field'
+
+puts 'Player 1: Enter Your Name'
+opponent_one = _input
+puts 'Player 2: Enter Your Name'
+opponent_two = _input
+
+puts "#{opponent_one} VS #{opponent_two}"
+
+puts '***** INSTRUCTION *****'
+puts "Enter the respective number to concure the space \n
+If one player write a straight line he/she/they would be a winner"
