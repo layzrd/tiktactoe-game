7b4687d4f04d5d7a4b9297d1902427c23dc465ad
alexoid <alex_zam@hotmail.es>
2020-07-28 16:09:48 -0500

    display board

--- bin/main.rb
+++ bin/main.rb
@@ -1,14 +1,28 @@
 #!/usr/bin/env ruby
 
 def _input
+
   inp = gets.chomp
-  unless inp
+  if inp.match(/[1-9]/)
+    return inp.to_i
+  else 
     puts 'Please enter a valid input'
-    getInput
+    
   end
   inp
 end
 
+def change_array (array,player)
+  move = _input-1
+  if move > 0 && move < 10
+
+    move_player=move
+    array[move_player]=player[0]
+    return print_score(array)
+  end
+end
+
+=begin
 def print_board(board)
   puts '***** #print_board start'
   board.each do |line|
@@ -19,6 +33,9 @@ def print_board(board)
   end
   puts '***** #print_board end'
 end
+=end
+
+ 
 
 puts 'Welcome to the Tic tac Toe field'
 
@@ -35,24 +52,37 @@ puts '***** INSTRUCTION *****'
 puts "Enter the respective number to concure the space \n
 If one player write a straight line he/she/they would be a winner \n"
 
+
 puts 'Here is the playing board select your move'
-puts print_board
+
 
 puts 'Enter Your Next Move'
-
-puts "Hooray! \n #{winner.name} win the game \n
+puts "Hooray! \n {winner.name} win the game \n
 If one player write a straight line he/she/they would be a winner"
 
 puts 'Here is the playing board select your move'
 puts 'Choose one of the spaces on the board'
 
-puts '1|2|3'
-puts '______'
-puts '4|5|6'
-puts '______'
-puts '7|8|9'
+array=[1,2,3,4,5,6,7,8,9]
+def print_score (array)
+  puts '----------------------------------'
+  puts "#{array[0]}|#{array[1]}|#{array[2]}"
+  puts '______'
+  puts "#{array[3]}|#{array[4]}|#{array[5]}"
+  puts '______'
+  puts "#{array[6]}|#{array[7]}|#{array[8]}"
+  puts '----------------------------------'
+end  
+
+#print_score(array)
+
 puts "#{opponent_one} make your move"
 
-puts print_board
+
+print_score (array)
+p change_array(array,opponent_one)
+
+
+#puts print_board 
 
 puts "Hooray! \n #{opponent_one} win the game"
