8d5c31536b20d828d1c0ba50aa2f0443a0a0e13d
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-07-28 22:29:55 +0300

    add .gitkeep in lib folder

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/env ruby
 
-print 'Hello world'
+def _input
+  inp = gets.chomp
+  unless inp
+    puts 'Please enter a valid input'
+    getInput
+  end
+  inp
+end
+
+puts 'Welcome to the Tic tac Toe field'
+
+puts 'Player 1: Enter Your Name'
+opponent_one = _input
+puts 'Player 2: Enter Your Name'
+opponent_two = _input
+
+puts '*********************'
+puts "#{opponent_one.capitalize} VS #{opponent_two.capitalize}"
+puts '*********************'
+
+puts '***** INSTRUCTION *****'
+puts "Enter the respective number to concure the space \n
+If one player write a straight line he/she/they would be a winner"
