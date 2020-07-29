f0db4060752a069cb11b3e5d085608fabcf6c451
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-07-29 20:39:51 +0300

    add testing for player class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,13 +2,33 @@ require 'rspec'
 require '../lib/player'
 
 RSpec.describe Player do
+  before do
+    subject.players.push('Ali')
+    subject.players.push('Alex')
+  end
   context 'when first created' do
     it 'should be instance of player class' do
       expect(subject).to be_instance_of Player
     end
 
-    it 'should have players array' do
+    it 'should have a :players array' do
       expect(subject.players).to be_a_kind_of Array
+    end
+
+    it 'next_player should return 0' do
+      expect(subject.next_player).to eql(0)
+    end
+  end
+
+  context '#icon' do
+    it 'should return an icon from * sign to #' do
+      expect { subject.next_player }.to change { subject.icon }.from('#').to('*')
+    end
+  end
+
+  context '#current_player' do
+    it 'shoul switch the player' do
+      expect { subject.next_player }.to change { subject.current_player }.from('Alex').to('Ali')
     end
   end
 end
